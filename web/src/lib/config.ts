097406efb7b67638c826bc9d6030ee66842c5072
kamireddyhari <hari.krishna119@gmail.com>
2026-09-04 22:35:06 +0530

    fix: update Apps Script URL to the working deployment ID

--- web/src/lib/config.ts
+++ web/src/lib/config.ts
@@ -1,3 +1,3 @@
 // BPL Arena - Central API Config
 // After you deploy the Google Apps Script, paste your Web App URL below
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzn38DeC_DiqwxOWHo0k1MdLWtKZJdFaEBuF98QZXmi592c2niJq6FeQbS6mDfoNHVAZw/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwx3L524UWsiswdxSVwQqvqNlHlY2yAm2dM-Ac5VJw-2n1fdQmuE1niVi99fDo5whaQ6w/exec";
